c1720c82c24db13b67869518a2d6ab45cdd94555
Sen Zhan <senzhanopt@gmail.com>
2025-09-29 14:01:05 +0200

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="grid_feedback_optimizer",        # package/distribution name
+    version="0.1.0",
+    packages=find_packages(),     # automatically find all submodules
+    install_requires=[            # dependencies you need for testing or core logic
+    ],
+    python_requires="==3.11.*"
+)
