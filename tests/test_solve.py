6b9b9efe2d162225b152ad4935820dd9d38476d3
Sen Zhan <senzhanopt@gmail.com>
2025-10-09 22:37:06 +0200

    add output to test solve

--- tests/test_solve.py
+++ tests/test_solve.py
@@ -2,6 +2,7 @@ from pathlib import Path
 import numpy as np
 from grid_feedback_optimizer.io.loader import load_network
 from grid_feedback_optimizer.engine.solve import solve
+from power_grid_model import ComponentType
 
 
 
@@ -11,7 +12,7 @@ def test_solve_from_example():
     EXAMPLE_JSON = Path(__file__).parent.parent / "examples" / "simple_example.json"
     # Load the network
     network = load_network(EXAMPLE_JSON)
-    output_data = solve(network)
+    output_data, gen_update, iterates = solve(network)
 
 def test_solve_from_example_with_transformer():
 
@@ -19,7 +20,7 @@ def test_solve_from_example_with_transformer():
     EXAMPLE_JSON = Path(__file__).parent.parent / "examples" / "simple_example_with_transformer.json"
     # Load the network
     network = load_network(EXAMPLE_JSON)
-    output_data = solve(network)
+    output_data, gen_update, iterates = solve(network)
     
 if __name__ == "__main__":
     test_solve_from_example()
