50c485123dea8dcf4dda058b58e154356ac2a6d2
Sen Zhan <s.zhan@tue.nl>
2025-10-10 16:46:59 +0200

    remove savefile from main

--- tests/test_main.py
+++ tests/test_main.py
@@ -2,8 +2,10 @@ from grid_feedback_optimizer.main import main
 
 def test_main_simple_example():
     # Example usage: user edits this line with their JSON path
-    main("./examples/simple_example.json", output_file='tests/output/optimized_simple_example.json')
+    res = main("./examples/simple_example.json")
+    res.save(output_file='tests/output/optimized_simple_example.json')
 
 def test_main_simple_example_with_transformer():
     # Example usage: user edits this line with their JSON path
-    main("./examples/simple_example_with_transformer.json", output_file='tests/output/optimized_simple_example_with_transformer.json')
+    res = main("./examples/simple_example_with_transformer.json")
+    res.save(output_file='tests/output/optimized_simple_example_with_transformer.json')
