e85dc0b8491258698fc169dab45df1e876aa23db
Sen Zhan <senzhanopt@gmail.com>
2025-11-12 12:13:12 +0100

    Add package __init__.py with complete public API

--- src/grid_feedback_optimizer/__init__.py
+++ src/grid_feedback_optimizer/__init__.py
@@ -1,1 +1,33 @@
+from .engine.grad_proj_optimizer import GradientProjectionOptimizer
+from .engine.powerflow import PowerFlowSolver
+from .engine.primal_dual_optimizer import PrimalDualOptimizer
+from .engine.renew_gen_projection import RenewGenProjection
+from .engine.solve import solve
 from .main import main
+from .models.loader import load_network, load_network_from_excel
+from .models.network import Bus, Line, Load, Network, RenewGen, Source, Transformer
+from .models.solve_data import OptimizationInputs, OptimizationModelData, SolveResults
+from .utils.utils import TransformerActivePowerTrackingCallback, network_to_model_data
+
+__all__ = [
+    "main",
+    "GradientProjectionOptimizer",
+    "PowerFlowSolver",
+    "PrimalDualOptimizer",
+    "RenewGenProjection",
+    "solve",
+    "load_network",
+    "load_network_from_excel",
+    "Bus",
+    "Line",
+    "Transformer",
+    "Source",
+    "RenewGen",
+    "Load",
+    "Network",
+    "OptimizationInputs",
+    "OptimizationModelData",
+    "SolveResults",
+    "TransformerActivePowerTrackingCallback",
+    "network_to_model_data",
+]
